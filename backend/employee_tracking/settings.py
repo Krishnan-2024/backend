932f8c502301aa0c0c43e084a17cd4e5200f7896
Sheik Abdullah <sivakrishnas785@gmail.com>
2025-04-10 14:58:45 +0530

    Added build.sh

--- backend/employee_tracking/settings.py
+++ backend/employee_tracking/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,7 +130,8 @@ AUTH_USER_MODEL = 'user_auth.User'
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/"static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_ROOT = BASE_DIR/"uploads"
 MEDIA_URl = "/media/"
